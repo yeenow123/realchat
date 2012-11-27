c15df11ea361f46894d4ddf149a5ee6106f7d62a
Yee-Ning Cheng <yee.ningc@gmail.com>
2012-11-27 17:08:35 -0500

    Remove extra create chat migration

--- db/migrate/20121127212311_create_chats_users.rb
+++
@@ -1,9 +0,0 @@
-class CreateChatsUsers < ActiveRecord::Migration
-  def change
-    create_table :chats_users do |t|
-      t.integer :chat_id
-      t.integer :user_id
-      t.timestamps
-    end
-  end
-end
